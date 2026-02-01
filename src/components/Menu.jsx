ae53b615a11ced9b304bab7f08febd5b8b7f86b2
Suprim Bishwakarma <suprimbishwakarma2002@gmail.com>
2026-02-01 13:15:13 +0545

    fix: minor reponsive-ness bugs

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -25,9 +25,8 @@ const Menu = () => {
 
       {/* Navigation Links */}
       <div
-        className={`${
-          isMenuOpen ? "flex" : "hidden"
-        } md:flex flex-col md:flex-row justify-center items-center gap-1 md:gap-2.5 absolute md:relative top-full left-0 w-full bg-white md:bg-transparent shadow-lg md:shadow-none z-50`}
+        className={`${isMenuOpen ? "flex" : "hidden"
+          } md:flex flex-col md:flex-row justify-center items-center gap-1 md:gap-2.5 absolute md:relative top-full left-0 w-full bg-white md:bg-transparent shadow-lg md:shadow-none z-50`}
       >
         {menus.map((menu) => (
           <div
@@ -36,12 +35,11 @@ const Menu = () => {
             onMouseEnter={() => setHovered(menu.id)}
             onMouseLeave={() => setHovered(null)}
           >
-            <button className="w-full md:w-auto px-4 py-3 bg-transparent text-black rounded-md cursor-pointer text-base font-medium flex justify-between items-center gap-2 transition-all duration-300">
+            <button className="w-full md:w-auto px-4 py-3 bg-transparent text-black md:text-white rounded-md cursor-pointer text-base font-medium flex justify-between items-center gap-2 transition-all duration-300">
               {menu.title}
               <span
-                className={`text-xs transition-transform duration-300 ${
-                  hovered === menu.id ? "rotate-180" : ""
-                }`}
+                className={`text-xs transition-transform duration-300 ${hovered === menu.id ? "rotate-180" : ""
+                  }`}
               >
                 ▼
               </span>
